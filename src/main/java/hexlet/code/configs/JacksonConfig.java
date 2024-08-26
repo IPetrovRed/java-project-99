0ab770bc5a088be65ad44b672b7b264dc70caff7
IPetrovRed <ipetrovdat@gmail.com>
2024-08-26 20:54:49 +0300

    reconfigure of DTO dir

--- src/main/java/hexlet/code/configs/JacksonConfig.java
+++ src/main/java/hexlet/code/configs/JacksonConfig.java
@@ -1,4 +1,4 @@
-package hexlet.code.config;
+package hexlet.code.configs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import org.openapitools.jackson.nullable.JsonNullableModule;
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
-public class Jackson {
+public class JacksonConfig {
     @Bean
-    public static Jackson2ObjectMapperBuilder objectMapperBuilder() {
+    Jackson2ObjectMapperBuilder objectMapperBuilder() {
         var builder = new Jackson2ObjectMapperBuilder();
         builder.serializationInclusion(JsonInclude.Include.NON_NULL)
                 .modulesToInstall(new JsonNullableModule());
