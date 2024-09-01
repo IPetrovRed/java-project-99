1987fbf769e1d99351c1818f449c19b563b7b501
IPetrovRed <ipetrovdat@gmail.com>
2024-09-01 18:14:51 +0300

    build gradle changes to fix build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,88 @@
 plugins {
-	application
+	java
+	jacoco
 	id("checkstyle")
-	id("jacoco")
-	id("io.freefair.lombok") version "8.6"
-	id("org.springframework.boot") version "3.2.4"
+	id("io.sentry.jvm.gradle") version "4.11.0"
+	application
+	id("org.springframework.boot") version "3.3.2-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.4"
-	id("com.github.ben-manes.versions") version "0.50.0"
+	id("io.freefair.lombok") version "8.4"
+
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
 application {
-	applicationName = "app"
-	mainClass.set("hexlet.code.AppApplication") }
+	mainClass.set("hexlet.code.AppApplication")
+}
 
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+tasks.jacocoTestReport {
+	reports { xml.required.set(true)
+	}
+}
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+sentry {
+	includeSourceContext = true
+
+	org = "student-em5"
+	projectName = "java"
+	authToken = System.getenv("SENTRY_AUTH_TOKEN")
+}
+
+tasks.sentryBundleSourcesJava {
+	enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	runtimeOnly("com.h2database:h2:2.2.224")
-	testImplementation("org.springframework.security:spring-security-test")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	compileOnly("org.projectlombok:lombok")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation(platform("org.junit:junit-bom:5.10.1"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	implementation("net.datafaker:datafaker:2.0.2")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
+	testImplementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-	finalizedBy(tasks.jacocoTestReport)
 }
 
-jacoco {
-	toolVersion = "0.8.11"
-}
-
-tasks.jacocoTestReport {
-	reports {
-		xml.required.set(true)
-	}
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
 }
