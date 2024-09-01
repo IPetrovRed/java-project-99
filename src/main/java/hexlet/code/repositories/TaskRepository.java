6af464d13ba01bf51a0be62fd30f78b995e96050
IPetrovRed <ipetrovdat@gmail.com>
2024-09-01 18:01:21 +0300

    working repos dir added

--- src/main/java/hexlet/code/repositories/TaskRepository.java
+++ src/main/java/hexlet/code/repositories/TaskRepository.java
@@ -1,6 +1,6 @@
 package hexlet.code.repositories;
 
-import hexlet.code.model.User;
+import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    Optional<User> findByEmail(String email);
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    Optional<Task> findByName(String name);
 }
