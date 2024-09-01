1e616da4845a7a2261ac4052c3b006598943745d
IPetrovRed <ipetrovdat@gmail.com>
2024-09-01 13:25:49 +0300

    componets rsa and init added

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,7 +1,6 @@
 package hexlet.code.component;
 
-import hexlet.code.dto.labels.LabelCreateDTO;
-import hexlet.code.dto.taskStatuses.TaskStatusCreateDTO;
+import hexlet.code.dto.taskStatuses.CreateDTO;
 import hexlet.code.dto.users.UserCreateDTO;
 import hexlet.code.services.LabelService;
 import hexlet.code.services.TaskStatusService;
@@ -32,14 +31,14 @@ public class DataInitializer implements ApplicationRunner {
         var user = new UserCreateDTO("user", "user", "user@gmail.com", "password");
         userService.create(user);
 
-        taskStatusService.create(new TaskStatusCreateDTO("Draft", "draft"));
-        taskStatusService.create(new TaskStatusCreateDTO("ToReview", "to_review"));
-        taskStatusService.create(new TaskStatusCreateDTO("ToBeFixed", "to_be_fixed"));
-        taskStatusService.create(new TaskStatusCreateDTO("ToPublish", "to_publish"));
-        taskStatusService.create(new TaskStatusCreateDTO("Published", "published"));
+        taskStatusService.create(new CreateDTO("Draft", "draft"));
+        taskStatusService.create(new CreateDTO("ToReview", "to_review"));
+        taskStatusService.create(new CreateDTO("ToBeFixed", "to_be_fixed"));
+        taskStatusService.create(new CreateDTO("ToPublish", "to_publish"));
+        taskStatusService.create(new CreateDTO("Published", "published"));
 
-        labelService.create(new LabelCreateDTO("bug"));
-        labelService.create(new LabelCreateDTO("feature"));
+        labelService.create(new hexlet.code.dto.labels.CreateDTO("bug"));
+        labelService.create(new hexlet.code.dto.labels.CreateDTO("feature"));
 
     }
 }
