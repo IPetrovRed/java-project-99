44f0188f31b58ab63c136592a1d7f854ef6074a1
IPetrovRed <ipetrovdat@gmail.com>
2024-09-10 21:50:44 +0300

    4-th attemp to solve proble. Return to @Data annotation in Label

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -8,19 +8,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
 import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.Objects;
+//import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "labels")
 @EntityListeners(AuditingEntityListener.class)
+@Data
 @NoArgsConstructor
 public class Label implements BaseEntity {
 
@@ -34,59 +37,11 @@ public class Label implements BaseEntity {
     @CreatedDate
     private LocalDate createdAt;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private Set<Task> tasks = new HashSet<>();
 
     public Label(Long id) {
         this.id = id;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Set<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(Set<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Label)) {
-            return false;
-        }
-        Label label = (Label) o;
-        return Objects.equals(id, label.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
+
